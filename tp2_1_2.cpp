8a87f95f6945f34b471e0a84de678d01eb14daf5
SantyVill <santyvillagra@gmail.com>
2020-04-04 22:47:52 -0300

    Agrego tp2_1_2 y .gitignore

--- tp2_1_2.cpp
+++ tp2_1_2.cpp
@@ -1,13 +1,23 @@
+#include <stdio.h>
 #define N 4
 #define M 5
-Int f,c;
-Double mt[N][M];
-…
-for(f = 0;f<N; f++)
-{
+
+int main(){
+    int f,c;
+    double mt[N][M]= {{ 4,5,6,2,1},
+                      { 1,8,3,5,7}, 
+                      { 7,2,9,8,3},
+                      { 6,9,1,5,2}};
+    double *pmat=&mt[0][0];
     for(f = 0;f<N; f++)
     {
-        cprintf(“%lf ”, mt[f][c]);
+        for(c = 0;c<M; c++)
+        {
+            printf("%lf  ",*(pmat+f*M+c));
+            //printf("%lf  ", mt[f][c]);
+        }
+        printf("\n");
     }
-    cprintf(“\n”);
+    getchar();
+    return 0;
 }
